f8b16668cc3fe307c86df52bed803e1c67ae9eca
Davanon <davanon2014@gmail.com>
2017-06-03 20:09:39 +0530

    first

---
+++ src/main/java/com/iqmsoft/spring/webflow/IncorrectLoginCredentialsException.java
@@ -0,0 +1,8 @@
+package com.iqmsoft.spring.webflow;
+
+
+public class IncorrectLoginCredentialsException extends Exception{
+
+    private static final long serialVersionUID = 3339021692474487380L;
+
+}
